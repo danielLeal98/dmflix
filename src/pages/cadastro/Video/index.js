d46ceb6f2784d0f179baadc4edea92482b843128
Daniel Matheus <danielmatheus2015@outlook.com>
2020-08-03 21:23:49 -0300

    Correção cadastro de Video

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -26,40 +26,21 @@ function CadastroVideo() {
       <form
         onSubmit={(event) => {
           event.preventDefault();
+          const categoriaEscolhida = categorias.find(
+            (categoria) => categoria.titulo === values.categoria
+          );
 
-          if (window.location.href.includes("localhost")) {
-            const categoriaEscolhida = categorias.find(
-              (categoria) => categoria.titulo === values.categoria
-            );
-
-            videosRepository
-              .create({
-                titulo: values.titulo,
-                url: values.url,
-                categoriaId: categoriaEscolhida.id,
-                createdAt: new Date(),
-              })
-              .then(() => {
-                alert("Vídeo cadastrado com sucesso em Homologação!");
-                history.push("/");
-              });
-          } else {
-            const categoriaEscolhida = categorias.find(
-              (categoria) => categoria.nome === values.categoria
-            );
-
-            videosRepository
-              .create({
-                nome: values.titulo,
-                url: values.url,
-                categoriaId: categoriaEscolhida.id,
-                createdAt: new Date(),
-              })
-              .then(() => {
-                alert("Vídeo cadastrado com sucesso em Produção!");
-                history.push("/");
-              });
-          }
+          videosRepository
+            .create({
+              titulo: values.titulo,
+              url: values.url,
+              categoriaId: categoriaEscolhida.id,
+              createdAt: new Date(),
+            })
+            .then(() => {
+              alert("Vídeo cadastrado com sucesso em Homologação!");
+              history.push("/");
+            });
         }}
       >
         <FormField
