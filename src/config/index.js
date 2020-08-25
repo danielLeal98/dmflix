bb21f9d311288d4bedd3b94affb587213634ada1
Daniel Matheus <danielmatheus2015@outlook.com>
2020-08-25 01:05:26 -0300

    update url

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
   ? 'http://localhost:3003'
-  : 'https://games-flix.herokuapp.com';
+  : 'https://bbb-teste.herokuapp.com';
 
 export default {
   URL_BACKEND,
