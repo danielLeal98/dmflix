ce400b9961658b41de225a1920c7ca50f9ccb4b4
Daniel Matheus <danielmatheus2015@outlook.com>
2020-08-25 01:06:01 -0300

    revertendo url projeto

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
   ? 'http://localhost:3003'
-  : 'https://bbb-teste.herokuapp.com';
+  : 'https://games-flix.herokuapp.com';
 
 export default {
   URL_BACKEND,
